178aa351dd3c0e53bfcd13631717fada487a2da3
nickforbizz <nickforbizz@gmail.com>
2019-08-05 19:43:07 +0300

    update

---
+++ js/all.js
@@ -0,0 +1,22 @@
+document.body.scrollTop = 0;
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("to-top ").style.display = "block ";
+        $(".nav").addClass("shadow");
+    } else {
+        document.getElementById("to-top ").style.display = "none ";
+        $(".nav").removeClass("shadow");
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
